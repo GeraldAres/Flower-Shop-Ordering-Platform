1bb95fceef40cccd4f85c4796e2972503e065325
Tre Alcoseba <a.lemuelvincent@gmail.com>
2025-11-26 17:14:57 +0800

    Made a ChckOut class

---
+++ src/FlowerOrderSystem/ChckOut.java
@@ -0,0 +1,16 @@
+package src.FlowerOrderSystem;
+
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.io.*;
+import java.time.*;
+
+public class ChckOut {
+    public static void main(String[] args) throws InvalidInputException {
+        LocalDateTime orderID = LocalDateTime.now();
+        User user;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yy hh:mm a");
+        String formatted = orderID.format(formatter);
+        System.out.println("Today's date is " + formatted);
+    }
+}
