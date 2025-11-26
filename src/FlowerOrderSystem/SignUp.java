a0016ac82c5cb28051502e9008a17150505ad0e5
Angela Jahziel Encabo <angelajahziel.encabo@cit.edu>
2025-11-26 22:46:42 +0800

    Made tweaks

---
+++ src/FlowerOrderSystem/SignUp.java
@@ -0,0 +1,16 @@
+package src.FlowerOrderSystem;
+
+public class SignUp {
+    private User user;
+    private String succesful = "Your Hiraya Cebu account has been created. Please refresh and log in to continue.";
+    public SignUp(String firstName, String lastName, String emailAddress, String contactNumber, String username, String password, String confirmPassword) throws InvalidInputException{
+        user = new User(firstName, lastName, emailAddress, contactNumber, username, password);
+    }
+
+    /*Will be displayed sa dialog box telling the user to refresh */
+    public String getSuccesful(){
+        return succesful;
+    }
+
+
+}
