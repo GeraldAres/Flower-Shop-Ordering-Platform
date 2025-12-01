9ece713a21d1aacff209462ea0ad63d72911ec80
jhendoe <gracefleime1@gmail.com>
2025-12-01 15:30:43 +0800

    sample pani

---
+++ src/FlowerOrderSystem/SampleViewOrderPanel.java
@@ -0,0 +1,14 @@
+package src.FlowerOrderSystem;
+
+import javax.swing.*;
+
+public class SampleViewOrderPanel {
+    private JPanel SampleViewOrderPanel;
+    private JPanel Holder;
+    private JPanel Header;
+    private JPanel BusinessName;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
