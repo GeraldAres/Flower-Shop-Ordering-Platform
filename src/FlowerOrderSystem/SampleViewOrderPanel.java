8c4c851829e590afc4b82ba50a97b82044b4a111
LAB15_USER ON WS26 <L15X15W26@LabHCI.net>
2025-12-01 17:44:07 +0800

    test

--- src/FlowerOrderSystem/SampleViewOrderPanel.java
+++ src/FlowerOrderSystem/SampleViewOrderPanel.java
@@ -1,7 +1,6 @@
 package src.FlowerOrderSystem;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class SampleViewOrderPanel {
 
@@ -9,7 +8,18 @@ public class SampleViewOrderPanel {
     private JPanel Header;
     private JPanel Holder;
     private JPanel BusinessName;
-    private JScrollPane Scroll;
+    private JPanel Body;
+    private JPanel North;
+    private JPanel West;
+    private JPanel South;
+    private JPanel East;
+    private JPanel Order;
+    private JScrollPane Scrikk;
+    private JPanel Status1;
+    private JPanel Date1;
+    private JPanel One;
+    private JPanel View1;
+    private JPanel Two;
     private JLabel LeftImg;
     private JLabel RightImg;
 
