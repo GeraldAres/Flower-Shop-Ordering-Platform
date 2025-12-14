86d90034383f95c18d47f2df6fe773ee0de14b3c
jhendoe <gracefleime1@gmail.com>
2025-12-15 06:38:22 +0800

    Continuation of Prototyping of Full Screen GUI Forms

--- src/FlowerOrderSystem/SampleOrderDetailsPnlFullScreen.java
+++ src/FlowerOrderSystem/SampleOrderDetailsPnlFullScreen.java
@@ -1,4 +1,28 @@
 package src.FlowerOrderSystem;
 
+import javax.swing.*;
+
 public class SampleOrderDetailsPnlFullScreen {
+    private JPanel container;
+    private JPanel header;
+    private JPanel center;
+    private JPanel left;
+    private JPanel Personal;
+    private JPanel orderInfo;
+    private JPanel OrdersHere;
+    private JPanel PersonalInformation;
+    private JLabel customerName;
+    private JLabel contactNumber;
+    private JLabel email;
+    private JPanel AddOns;
+    private JLabel fereroPrice;
+    private JLabel labubuPrice;
+    private JLabel tobleronPrice;
+    private JLabel teddyBearPrice;
+    private JPanel orderSummaryPanel;
+    private JPanel right;
+    private JTextArea specialRequest;
+    private JLabel totalPrice;
+    private JButton checkoutBtn;
+    private JPanel SampleOrderDetailsPnlFllScrn;
 }
--- src/FlowerOrderSystem/SampleSignUpPnlFullScreen.java
+++ src/FlowerOrderSystem/SampleSignUpPnlFullScreen.java
@@ -1,4 +1,31 @@
 package src.FlowerOrderSystem;
 
+import javax.swing.*;
+
 public class SampleSignUpPnlFullScreen {
+    private JPanel Holder;
+    private JPanel BusinessName;
+    private JPanel Left;
+    private JTextField passwordField;
+    private JButton createAccountButton;
+    private JLabel LogInBtn;
+    private JLabel invalidUsername;
+    private JLabel passwordMismatch;
+    private JLabel email;
+    private JTextField emailField;
+    private JLabel ContactNo;
+    private JTextField contactField;
+    private JLabel usernameText;
+    private JTextField usernameField;
+    private JLabel ConfirmPassField;
+    private JTextField confirmPasswordField;
+    private JTextField fullnameField;
+    private JLabel invalidNameField;
+    private JLabel invalidEmail;
+    private JLabel invalidNumber;
+    private JLabel weakPassword;
+    private JPanel Right;
+    private JLabel Welcome;
+    private JButton prevButton;
+    private JPanel SampleSignUpPnlFllScrn;
 }
